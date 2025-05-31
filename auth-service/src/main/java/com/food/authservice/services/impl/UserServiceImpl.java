2e9eb3241130ad8d438486b916496d60f37a056f
Fabio-Alfredo <00210121@uca.edu.sv>
2025-05-31 13:52:12 -0600

    :sparkles: add: new functions for generate and validate token

--- auth-service/src/main/java/com/food/authservice/services/impl/UserServiceImpl.java
+++ auth-service/src/main/java/com/food/authservice/services/impl/UserServiceImpl.java
@@ -2,24 +2,57 @@ package com.food.authservice.services.impl;
 
 import com.food.authservice.domains.models.Token;
 import com.food.authservice.domains.models.User;
+import com.food.authservice.exceptions.HttpError;
+import com.food.authservice.repositorie.UserRepository;
 import com.food.authservice.services.contract.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public User findByIdentifier(String identifier) {
-        return null;
+        try{
+            User user = userRepository.findByEmailOrUsername(identifier, identifier);
+            if (user == null) {
+                throw new HttpError(HttpStatus.NOT_FOUND, "User not found with identifier: " + identifier);
+            }
+            return user;
+        }catch (HttpError e){
+            throw e;
+        }
     }
 
     @Override
     public User findByEmail(String email) {
-        return null;
+        try {
+            User user = userRepository.findByEmail(email);
+            if (user == null) {
+                throw new HttpError(HttpStatus.NOT_FOUND, "User not found with email: " + email);
+            }
+            return user;
+        } catch (HttpError e) {
+            throw e;
+        }
     }
 
     @Override
     public Token registerToken(User user) {
-        return null;
+        try{
+            cleanTokens(user);
+            //generar token
+            Token token = new Token();
+            return token;
+        }catch (HttpError e) {
+            throw e;
+        }
     }
 
     @Override
